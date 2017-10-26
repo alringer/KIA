3e9768ea4ccb55e473a3d6838be13b3bb04406b4
Brian Burkett <brianburkett@me.com>
2017-10-26 12:06:48 -0700

    build out poc

--- build/scripts/registration.js
+++ build/scripts/registration.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
         }
       },
       // Toggle the disabled state for form buttons based on required critera when the form is interactied with
-      _toggle_disable: function() {
+      _toggle_disable: function _toggle_disable() {
         var $this = $(this);
         // For the vehicle lookup form
         if (window.registration.elements.registration_form.is('.vehicle-lookup-form')) {
