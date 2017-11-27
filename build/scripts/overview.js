5a2a24aaca10dc0f7db00fc19e19b2d49e74ccfd
Brian Burkett <brianburkett@me.com>
2017-11-26 18:11:30 -0800

    modal work

--- build/scripts/overview.js
+++ build/scripts/overview.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+
+$(document).ready(function () {
+  window.overview = {
+    elements: {
+      add_meta_submit: $('.add-meta-field-submit'),
+      add_meta_input: $('.add-meta-field-input')
+    },
+    methods: {
+      _meta_form_validation: function _meta_form_validation() {
+        var $this = $(this);
+        if ($this.val().length) {
+          window.overview.elements.add_meta_submit.removeClass('disabled');
+        } else {
+          window.overview.elements.add_meta_submit.addClass('disabled');
+        }
+      }
+    }
+  };
+  // EVENTS
+  window.overview.elements.add_meta_input.on('keyup', window.overview.methods._meta_form_validation);
+});
---
+++ source/scripts/overview.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  window.overview = {
+    elements : {
+      add_meta_submit : $('.add-meta-field-submit'),
+      add_meta_input : $('.add-meta-field-input'),
+    },
+    methods : {
+      _meta_form_validation : function() {
+        var $this = $(this);
+        if($this.val().length) {
+          window.overview.elements.add_meta_submit.removeClass('disabled');
+        }else{
+          window.overview.elements.add_meta_submit.addClass('disabled');
+        }
+      },
+    }
+  };
+  // EVENTS
+  window.overview.elements.add_meta_input.on('keyup', window.overview.methods._meta_form_validation);
+});
