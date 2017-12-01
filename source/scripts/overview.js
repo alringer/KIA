5bdbced12a3dcc98fffb12a2ce93d0af88f8c869
Brian Burkett <brianburkett@me.com>
2017-12-01 14:15:05 -0800

    disabled button

--- source/scripts/overview.js
+++ source/scripts/overview.js
@@ -3,6 +3,8 @@ $(document).ready(() => {
     elements : {
       add_meta_submit : $('.add-meta-field-submit'),
       add_meta_input : $('.add-meta-field-input'),
+      set_dealer_radio : $('[name="preferred-dealer"]'),
+      dealer_submit : $('.preferred-dealer-submit'),
     },
     methods : {
       _meta_form_validation : function() {
@@ -13,8 +15,16 @@ $(document).ready(() => {
           window.overview.elements.add_meta_submit.addClass('disabled');
         }
       },
+      _dealer_validation : function() {
+        if(window.overview.elements.set_dealer_radio.filter(':checked').length) {
+          window.overview.elements.dealer_submit.removeClass('disabled');
+        }else{
+          window.overview.elements.dealer_submit.addClass('disabled');
+        }
+      },
     }
   };
   // EVENTS
   window.overview.elements.add_meta_input.on('keyup', window.overview.methods._meta_form_validation);
+  window.overview.elements.set_dealer_radio.on('change', window.overview.methods._dealer_validation);
 });
