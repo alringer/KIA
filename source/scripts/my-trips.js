d1b27d3c941742cd9096f7c659df5a0f9d9514ca
Brian Burkett <brianburkett@me.com>
2018-01-24 23:05:57 -0800

    updates

--- source/scripts/my-trips.js
+++ source/scripts/my-trips.js
@@ -1,14 +1,28 @@
 $(document).ready(() => {
   window.myTrips = {
     elements : {
+      view : $('.my-trips-page-view'),
       trip : $('.trip'),
       trip_collapse : $('.trip .collapse'),
       badge_input : $('.trip .badge input'),
       badge_cancel : $('.trip .badge em'),
       trip_list_scroll : $('.trips-list-inner'),
       scroll_top : $('.scroll-top'),
+      stop_propagation : $('.stop-propagation'),
+      start_edit : $('.start-edit'),
+      stop_edit : $('.stop-edit'),
+      toggle_edit : $('.toggle-edit'),
+      trip_checkboxes : $('.trip .checkbox input'),
+      merge_buttons_wrap : $('.merge-toggle'),
+      merge : $('.merge-toggle .merge'),
+      unmerge : $('.merge-toggle .unmerge'),
+      delete : $('.edit-toolbar .delete'),
+      select_all : $('.select-all'),
     },
     methods : {
+      _stop_prop : (e) => {
+        e.stopPropagation()
+      },
       _toggle : function() {
         var $this = $(this),
             $collapse = $this.find('.collapse');
@@ -108,6 +122,91 @@ $(document).ready(() => {
           window.myTrips.elements.trip_list_scroll.scrollTop(0);
         },
       },
+      edit : {
+        _start : () => {
+          window.myTrips.elements.view.addClass('editing');
+        },
+        _stop : () => {
+          window.myTrips.elements.view.removeClass('editing');
+          window.myTrips.elements.trip_checkboxes.prop('checked', false);
+          window.myTrips.elements.select_all.prop('checked', false);
+        },
+        _checkbox_change : () => {
+          var $checkboxes = window.myTrips.elements.trip_checkboxes.filter(':checked');
+          if($checkboxes.length > 1) {
+            window.myTrips.elements.merge_buttons_wrap.removeClass('disabled');
+          }else{
+            window.myTrips.elements.merge_buttons_wrap.addClass('disabled');
+          }
+          if($checkboxes.length) {
+            window.myTrips.elements.delete.removeClass('disabled');
+          }else{
+            window.myTrips.elements.delete.addClass('disabled');
+          }
+          var merge = true;
+          if($checkboxes.length === 1) {
+            if($checkboxes.parents('.trip.merged').length) {
+              window.myTrips.elements.merge_buttons_wrap.removeClass('disabled');
+              merge = false;
+            }
+          }
+          var $btns = window.myTrips.elements.merge_buttons_wrap.find('span');
+          $btns.removeClass('active');
+          if(merge) {
+            $btns.filter('.merge').addClass('active');
+          }else{
+            $btns.filter('.unmerge').addClass('active');
+          }
+        },
+        _merge : () => {
+          var $trips = window.myTrips.elements.trip_checkboxes.filter(':checked').parents('.trip');
+          $trips.addClass('merged');
+          $trips.not('.active').each(function(){
+            var $this = $(this);
+            $this.css('height', $this.outerHeight());
+            setTimeout(() => {
+              $this.addClass('hide-trip');
+            }, 10);
+          });
+          window.myTrips.elements.trip_checkboxes.prop('checked', false);
+          window.myTrips.methods.edit._checkbox_change();
+        },
+        _delete : () => {
+          var $trips = window.myTrips.elements.trip_checkboxes.filter(':checked').parents('.trip');
+          $trips.each(function(){
+            var $this = $(this);
+            $this.css('height', $this.outerHeight());
+            setTimeout(() => {
+              $this.addClass('hide-trip');
+            }, 10);
+          });
+          window.myTrips.elements.trip_checkboxes.prop('checked', false);
+          window.myTrips.methods.edit._checkbox_change();
+        },
+        _unmerge : () => {
+          var $trips = window.myTrips.elements.trip;
+          $trips.removeClass('merged hide-trip');
+          setTimeout(() => {
+            $trips.css('height', 'auto');
+          }, 550);
+          window.myTrips.methods.edit._checkbox_change();
+        },
+        _select_all : function() {
+          var $this = $(this),
+              checked = $this.prop('checked');
+          window.myTrips.elements.select_all.prop('checked', checked);
+          window.myTrips.elements.trip_checkboxes.each(function() {
+            var $this = $(this),
+                $trip = $this.parents('.trip');
+            if($trip.hasClass('hide-trip')) {
+              return;
+            }else{
+              $this.prop('checked', checked);
+            }
+          });
+          window.myTrips.methods.edit._checkbox_change();
+        }
+      },
     }
   };
   // EVENTS
@@ -117,4 +216,13 @@ $(document).ready(() => {
   $('body').delegate('.trip .badge em', 'click', window.myTrips.methods.tags._cancel);
   window.myTrips.elements.trip_list_scroll.on('scroll', window.myTrips.methods.scroll.onscroll);
   window.myTrips.elements.scroll_top.on('click', window.myTrips.methods.scroll.scrolltop);
+  window.myTrips.elements.stop_propagation.on('click', window.myTrips.methods._stop_prop);
+  window.myTrips.elements.start_edit.on('click', window.myTrips.methods.edit._start);
+  window.myTrips.elements.stop_edit.on('click', window.myTrips.methods.edit._stop);
+  window.myTrips.elements.trip_checkboxes.on('change', window.myTrips.methods.edit._checkbox_change);
+  window.myTrips.elements.merge.on('click', window.myTrips.methods.edit._merge);
+  window.myTrips.elements.unmerge.on('click', window.myTrips.methods.edit._unmerge);
+  window.myTrips.elements.delete.on('click', window.myTrips.methods.edit._delete);
+  window.myTrips.elements.select_all.on('change', window.myTrips.methods.edit._select_all);
+  console.log(window.myTrips.elements.select_all.length);
 });
