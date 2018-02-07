f4b01efa5d58c756d48d1c8e6168d70c48a19419
Brian Burkett <brianburkett@me.com>
2018-02-06 23:54:16 -0800

    prelogin

--- source/scripts/pre-login.js
+++ source/scripts/pre-login.js
@@ -2,6 +2,8 @@ $(document).ready(() => {
   window.preLogin = {
     elements : {
       status : $('.status'),
+      nav_scroll : $('nav.scroll'),
+      nav_scroll_trigger : $('.trigger-nav-scroll'),
     },
     methods : {
       _satus_scroll : () => {
@@ -11,13 +13,18 @@ $(document).ready(() => {
         var status_start = window.preLogin.elements.status.offset().top,
             y = window.pageYOffset,
             window_height = window.innerHeight;
-        console.log(y);
         if(y > (status_start - (window_height * .75))) {
           window.preLogin.elements.status.addClass('play')
+        }
+        if(y > window.preLogin.elements.nav_scroll_trigger.offset().top) {
+          window.preLogin.elements.nav_scroll.addClass('show');
+        }else{
+          window.preLogin.elements.nav_scroll.removeClass('show');
         }
       },
     }
   };
+  console.log(window.preLogin.elements.nav_scroll.length);
   // EVENTS
   $(window).on('scroll', window.preLogin.methods._satus_scroll);
 });
