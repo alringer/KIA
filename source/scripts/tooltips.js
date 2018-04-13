a03edf24650645c429693bd60250d89ab35609b0
Brian Burkett <brianburkett@me.com>
2018-04-13 15:45:56 -0700

    tooltip close

--- source/scripts/tooltips.js
+++ source/scripts/tooltips.js
@@ -2,13 +2,26 @@ $(document).ready(() => {
   window.tooltips = {
     elements : {
       tooltip : $('[data-toggle="tooltip"]'),
+      warranty_tip : $('.view-warranty-info [data-toggle="tooltip"]'),
     },
     methods : {
       _init : () => {
-        window.tooltips.elements.tooltip.tooltip();
+        window.tooltips.elements.tooltip.not('.special').tooltip();
+
+        window.tooltips.elements.warranty_tip.tooltip({
+          html: true,
+          trigger: "click",
+          viewport: ".text-content",
+        });
       },
+      _close : function() {
+        $(this).parents('.tooltip').prev('[data-toggle="tooltip"]').trigger('click');
+      }
     }
   };
+
+  $('body').delegate('.tooltip-close', 'click', window.tooltips.methods._close);
+
   // EVENTS
   window.tooltips.methods._init();
 });
