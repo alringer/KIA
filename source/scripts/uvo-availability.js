ad4ba18bc7b86d335d9465595bcee08a3264cc51
Enrique Vega <evega@seamgen.com>
2020-03-11 10:24:46 -0700

    Finished KK-869, KK-868

--- source/scripts/uvo-availability.js
+++ source/scripts/uvo-availability.js
@@ -1,44 +1,22 @@
 $(document).ready(() => {
-    // window.allVehicles = {
-    //     elements : {
-    //         vehicle_images : $('.vehicle-image'),
-    //         add_active_rectangle : $('.active-rectangle'),
-    //         vehicle_wrapper : $('.js-vehicle-wrapper')
-    //     },
-    //     methods : {
-    //         editing : {
-    //             _select_vehicle : () => {
-    //                 // e.stopPropagation();
-    //                 window.allVehicles.elements.add_active_rectangle.addClass('active')
-    //             },
-    //             _deselect_vehicle : () => {
-    //                 // e.stopPropagation();
-    //                 window.allVehicles.elements.add_active_rectangle.removeClass('active')
-    //             },
-    //             _toggle_vehicle : () => {
-    //                 // e.stopPropagation();
-    //                 $(this).addClass('active')
-    //                 console.log("this got clicked!", $(this));
-                    
-    //                 // if(window.allVehicles.elements.add_active_rectangle.hasClass('active')) {
-    //                 //     window.allVehicles.methods.editing._deselect_vehicle();
-    //                 // } else {
-    //                 //     window.allVehicles.methods.editing._select_vehicle();
-    //                 // }
-    //             }
-    //         }
-    //     }
-    // }
-    // // EVENTS
-    // window.allVehicles.elements.vehicle_wrapper.on('click', window.allVehicles.methods.editing._toggle_vehicle)
-    // window.allVehicles.elements.vehicle_images.on('click', window.allVehicles.methods.editing._deselect_vehicle)
-    //     window.allVehicles.elements.vehicle_images.toggle(
-    //         window.allVehicles.methods.editing._deselect_vehicle,
-    //         window.allVehicles.methods.editing._select_vehicle
-    //     )
-
-        $('.js-vehicle-wrapper').click(function () {
-            $('.js-vehicle-wrapper').removeClass('active')
-            $(this).addClass('active')
-        })
+    window.allVehicles = {
+        elements : {
+            vehicle_images : $('.vehicle-image'),
+            add_active_rectangle : $('.active-rectangle'),
+            vehicle_wrapper : $('.js-vehicle-wrapper')
+        },
+        methods : {
+            editing : {
+                _toggle_vehicle : (e) => {
+                    var $this = $(e.target)
+                    window.allVehicles.elements.vehicle_wrapper.removeClass('active')
+                    $this.addClass('active')
+                    window.allVehicles.elements.vehicle_wrapper.addClass('inactive')
+                    $this.removeClass('inactive')
+                }
+            }
+        }
+    }
+    // EVENTS
+    window.allVehicles.elements.vehicle_wrapper.click(window.allVehicles.methods.editing._toggle_vehicle)
 })
